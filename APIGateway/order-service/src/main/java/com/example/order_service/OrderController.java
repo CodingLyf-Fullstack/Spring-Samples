e0821b93833256c997f5b8b1a4b9bd67a86244aa
CodingLyf <codeandcarbs@gmail.com>
2025-10-12 22:37:23 +0530

    API gateway sample

---
+++ APIGateway/order-service/src/main/java/com/example/order_service/OrderController.java
@@ -0,0 +1,16 @@
+package com.example.order_service;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/orders")
+public class OrderController {
+
+    @PostMapping
+    public String placeOrder(@RequestBody String product) {
+        return "Order placed for: " + product;
+    }
+}
